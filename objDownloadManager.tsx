8a7f7fdd42d27963ae284457b3447af574459ef6
Samuel Watson <planetbridging@gmail.com>
2023-11-07 23:30:13 +1100

    fixed cpelookup my god its even better then before

--- objDownloadManager.tsx
+++ objDownloadManager.tsx
@@ -22,8 +22,75 @@ function extractCPEMatches(nodes) {
   return cpeMatches;
 }
 
+const SEPARATOR = "-><-"; // Custom separator to avoid conflict with dots in values
+
+function mapUniqueKeys(obj, map = new Map(), currentPath = []) {
+  // Iterate over each property in the object
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      // Handle the case where the property is another object or an array
+      if (typeof obj[key] === "object" && obj[key] !== null) {
+        mapUniqueKeys(obj[key], map, currentPath.concat(key));
+      } else {
+        // Handle the case where the property is a value
+        const path = currentPath.concat(key).join(SEPARATOR);
+        if (!map.has(path)) {
+          map.set(path, new Set()); // Use Set to avoid duplicates
+        }
+        // If the value is an array, add each element to the Set
+        if (Array.isArray(obj[key])) {
+          obj[key].forEach((item) => map.get(path).add(item));
+        } else {
+          map.get(path).add(obj[key]);
+        }
+      }
+    }
+  }
+
+  // If the currentPath is empty, we are at the root, return the combined map
+  if (currentPath.length === 0) {
+    const combinedMap = new Map();
+    for (const [path, valuesSet] of map.entries()) {
+      // Extract the key from the path, considering custom separator
+      const key = path.split(SEPARATOR).pop();
+      if (!combinedMap.has(key)) {
+        combinedMap.set(key, new Set());
+      }
+      // Combine Sets of values
+      valuesSet.forEach((value) => combinedMap.get(key).add(value));
+    }
+    // Convert Sets to Arrays
+    for (const [key, valueSet] of combinedMap.entries()) {
+      combinedMap.set(key, Array.from(valueSet));
+    }
+    return combinedMap;
+  }
+
+  return map;
+}
+
 export class objDownloadManager {
-  constructor() {}
+  constructor() {
+    /*const json = {
+      a: 1,
+      b: {
+        c: 2,
+        d: {
+          e: 3,
+        },
+      },
+      f: {
+        c: 4,
+      },
+    };
+
+    const uniqueKeysMap = mapUniqueKeys(json);
+
+    // Logging the result
+    uniqueKeysMap.forEach((value, key) => {
+      console.log(key, value);
+    });*/
+  }
 
   async startup() {
     const downloadFolder = "./download";
@@ -96,22 +163,17 @@ export class objDownloadManager {
       const cveMeta = cveItem.cve.CVE_data_meta;
       const cveId = cveMeta.ID;
       const descriptionTmp = cveItem.cve.description.description_data[0]?.value;
-      const cpeMatches = extractCPEMatches(cveItem.configurations.nodes);
+      //const cpeMatches = extractCPEMatches(cveItem.configurations.nodes);
+      const cpeMatches = cveItem.configurations.nodes[0]?.cpe_match;
 
       if (!cveId || !descriptionTmp || !cpeMatches) {
         //console.log(`Invalid structure for CVE item: ${cveId}. Skipping.`);
         continue;
       }
 
-      /*if (cveId == "CVE-2017-0144") {
-        console.log(cveItem);
-        console.log(cveItem.configurations.nodes);
-        
-      }*/
-
       //const cpeValues = cpeMatches.map((cpeMatch) => cpeMatch.cpe23Uri);
 
-      const cpeValues = cpeMatches.map((cpeMatch) => {
+      var cpeValues = cpeMatches.map((cpeMatch) => {
         const cpeParts = cpeMatch.cpe23Uri.split(":").slice(2);
         //var tmp1 =  cpeParts.join(":");
         var rebuild = "";
@@ -142,8 +204,50 @@ export class objDownloadManager {
         console.log(`Impact details not available for CVE item: ${cveId}`);
         continue;
       }
-      //console.log(cveItem.impact);
 
+      const uniqueKeysMap = mapUniqueKeys(cveItem);
+
+      // Logging the result
+
+      //if (cveId == "CVE-2017-0144") {
+      //console.log(cveItem);
+      //console.log(cveItem.configurations.nodes);
+      /*uniqueKeysMap.forEach((value, key) => {
+          console.log(key, value);
+        });*/
+      if (uniqueKeysMap.has("cpe23Uri")) {
+        var lstCpe = uniqueKeysMap.get("cpe23Uri");
+        for (var c in lstCpe) {
+          const cpeParts = lstCpe[c].split(":").slice(2);
+          //var tmp1 =  cpeParts.join(":");
+          var rebuild = "";
+
+          for (var t in cpeParts) {
+            if (cpeParts[t] != "-" && cpeParts[t] != "*") {
+              rebuild += cpeParts[t] + ":";
+            }
+          }
+          rebuild = rebuild.slice(0, -1);
+
+          if (objCollectorYear.lstUniqCpe.has(rebuild)) {
+            var lstTmp = objCollectorYear.lstUniqCpe.get(rebuild);
+            if (!lstTmp.includes(cveId)) {
+              lstTmp.push(cveId);
+              objCollectorYear.lstUniqCpe.set(rebuild, lstTmp);
+            }
+          } else {
+            objCollectorYear.lstUniqCpe.set(rebuild, [cveId]);
+          }
+
+          if (!cpeValues.includes(rebuild)) {
+            cpeValues.push(rebuild);
+          }
+          //console.log(rebuild);
+        }
+      }
+      //}
+      //console.log(cveItem.impact);
+      //console.log(cpeValues);
       var categories = this.getCategories(descriptionTmp);
       var objCve = {
         baseMetricV2: cveItem.impact?.baseMetricV2,
@@ -154,6 +258,10 @@ export class objDownloadManager {
         categories: categories,
       };
       objCollectorYear.lstCve.set(cveId, objCve);
+
+      /*if (cveId == "CVE-2017-0144") {
+        console.log(cpeValues);
+      }*/
       //console.log(objCve);
       //break;
     }
